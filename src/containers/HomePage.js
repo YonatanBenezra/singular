05cdc2b022109a986d67423bdfdf3c4bf0f18fae
hafizbali315 <hafizbali315@gmail.com>
2022-01-09 05:13:18 +0500

    Home Page Animations

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -4,27 +4,36 @@ import SingularLogo from '../assets/SingularLogo.png'
 import WelcomePageBall from '../assets/WelcomePageBall.png'
 
 const HomePage = () => {
+	const welcomeBallVariants = {
+		visible: {
+			x: [0, 0, -100, -50, 100, 50, 0],
+			y: [0, 600, 400, 600, 400, 600, 0],
+
+			rotate: 360,
+
+			transition: {
+				duration: 5,
+				rotate: {
+					duration: 1,
+					repeat: Infinity,
+					type: 'tween',
+					ease: 'linear',
+				},
+
+				delay: 1,
+			},
+		},
+	}
+
 	const ballVariants = {
 		visible: {
 			rotate: 360,
 
 			transition: {
 				repeat: Infinity,
-				repeatType: 'loop',
-				repeatDelay: 0,
+				type: 'tween',
+				ease: 'linear',
 				duration: 1,
-			},
-		},
-	}
-
-	const welcomeBallVariants = {
-		visible: {
-			rotate: 360,
-			transition: {
-				repeat: Infinity,
-				repeatType: 'loop',
-				repeatDelay: 0,
-				duration: 0.8,
 			},
 		},
 	}
@@ -45,6 +54,22 @@ const HomePage = () => {
 		},
 	}
 
+	const fadeFromLeft = {
+		hidden: {
+			x: -1000,
+		},
+
+		visible: {
+			x: 0,
+
+			transition: {
+				type: 'spring',
+				stiffness: 70,
+				delay: 6,
+			},
+		},
+	}
+
 	return (
 		<div className="home_page">
 			<div className="navbar">
@@ -60,6 +85,11 @@ const HomePage = () => {
 				<motion.h2 variants={textVariants} initial="hidden" animate="visible">
 					Take a minute to play them freely
 				</motion.h2>
+
+				<motion.button variants={fadeFromLeft} className="play_button" initial="hidden" animate="visible">
+					<motion.img variants={ballVariants} src={WelcomePageBall} alt="WelcomePageBall" />
+					LET'S GO!
+				</motion.button>
 
 				<p>© 2022 | Singgular.io | All rights reserved | Let's talk- Info@singgular.io</p>
 			</div>
